4ceb302648ba9e033ab84950421f70dac5f85c5e
Anzh <anzhrai@gmail.com>
2021-03-06 01:32:45 +0530

    Web Page Completed

--- app/main.py
+++ app/main.py
@@ -1,34 +1,72 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request
+from flask_cors import CORS
 import pickle
 import numpy as np
 from PIL import Image
-from tensorflow.keras.models import load_model
+import tflite_runtime.interpreter as tflite
 
 
 
+### Load Models
+
+## Malaria
+#Load Model
+interpreter_malaria = tflite.Interpreter(model_path='models/malaria.tflite')
+interpreter_malaria.allocate_tensors()
+#get input and output tensors
+input_details = interpreter_malaria.get_input_details()
+output_details = interpreter_malaria.get_output_details()
+
+## Pneumonia
+# Load Model
+interpreter_pneumonia = tflite.Interpreter(model_path='models/pneumonia.tflite')
+interpreter_pneumonia.allocate_tensors()
+#get input and output tensors
+input_details = interpreter_pneumonia.get_input_details()
+output_details = interpreter_pneumonia.get_output_details()
+
+## Diabetes
+# Load Model
+model_diabetes = pickle.load(open('models/diabetes.pkl','rb'))
+
+## Breast Cancer
+# Load Model
+model_breast = pickle.load(open('models/breast_cancer.pkl','rb'))
+
+## Heart
+# Load Model
+model_heart = pickle.load(open('models/heart.pkl','rb'))
+
+## Kidney
+# Load Model
+model_kidney = pickle.load(open('models/kidney.pkl','rb'))
+
+## Liver
+# Load Model
+model_liver = pickle.load(open('models/liver.pkl','rb'))
+
+
+# Define App
 app = Flask(__name__)
+CORS(app)
+
 
 def predict(values, dic):
     if len(values) == 8:
-        model = pickle.load(open('models/diabetes.pkl','rb'))
         values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
+        return model_diabetes.predict(values.reshape(1, -1))[0]
     elif len(values) == 26:
-        model = pickle.load(open('models/breast_cancer.pkl','rb'))
         values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
+        return model_breast.predict(values.reshape(1, -1))[0]
     elif len(values) == 13:
-        model = pickle.load(open('models/heart.pkl','rb'))
         values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
+        return model_heart.predict(values.reshape(1, -1))[0]
     elif len(values) == 18:
-        model = pickle.load(open('models/kidney.pkl','rb'))
         values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
+        return model_kidney.predict(values.reshape(1, -1))[0]
     elif len(values) == 10:
-        model = pickle.load(open('models/liver.pkl','rb'))
         values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
+        return model_liver.predict(values.reshape(1, -1))[0]
 
 @app.route("/")
 def home():
@@ -77,23 +115,37 @@ def predictPage():
 
 @app.route("/malariapredict", methods = ['POST', 'GET'])
 def malariapredictPage():
+    pred = 1
+
     if request.method == 'POST':
         try:
             if 'image' in request.files:
                 img = Image.open(request.files['image'])
                 img = img.resize((36,36))
                 img = np.asarray(img)
+                
+                if img.shape[2] == 4:
+                    img = img[:, :, :3]
+
                 img = img.reshape((1,36,36,3))
-                img = img.astype(np.float64)
-                model = load_model("models/malaria.h5")
-                pred = np.argmax(model.predict(img)[0])
-        except:
-            message = "Please upload an Image"
+                img = img.astype(np.float32)
+
+                #get prediction
+                interpreter_malaria.set_tensor(input_details[0]['index'], img)
+                interpreter_malaria.invoke()
+                output_data = interpreter_malaria.get_tensor(output_details[0]['index'])
+                pred = np.argmax(output_data[0])
+        except Exception as ex:
+            print(ex)
+            message = "Please upload a valid Image"
             return render_template('malaria.html', message = message)
+
     return render_template('malaria_predict.html', pred = pred)
 
 @app.route("/pneumoniapredict", methods = ['POST', 'GET'])
 def pneumoniapredictPage():
+    pred = 0
+
     if request.method == 'POST':
         try:
             if 'image' in request.files:
@@ -101,11 +153,17 @@ def pneumoniapredictPage():
                 img = img.resize((36,36))
                 img = np.asarray(img)
                 img = img.reshape((1,36,36,1))
+                img = img.astype(np.float32)
                 img = img / 255.0
-                model = load_model("models/pneumonia.h5")
-                pred = np.argmax(model.predict(img)[0])
-        except:
-            message = "Please upload an Image"
+                
+                #get prediction
+                interpreter_pneumonia.set_tensor(input_details[0]['index'], img)
+                interpreter_pneumonia.invoke()
+                output_data = interpreter_pneumonia.get_tensor(output_details[0]['index'])
+                pred = np.argmax(output_data[0])
+        except Exception as ex:
+            print(ex)
+            message = "Please upload a valid Image"
             return render_template('pneumonia.html', message = message)
     return render_template('pneumonia_predict.html', pred = pred)
 
